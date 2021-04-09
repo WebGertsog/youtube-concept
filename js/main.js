45427ec6a59ac558aacebc05540369b8efe924e6
WebGertsog <82025125+WebGertsog@users.noreply.github.com>
2021-04-09 18:05:44 +0300

    Пофиксили меню и шапку, добавили контент

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.channel-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:6,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next',
+      prevEl: '.channel-button-prev',
+    },
+  
+  });
+  const swiper_2 = new Swiper('.recommended-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:3,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.recommended-button-next',
+      prevEl: '.recommended-button-prev',
+    },
+  
+  });
+  const swiper_3 = new Swiper('.food-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:6,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.food-button-next',
+      prevEl: '.food-button-prev',
+    },
+  
+  });
